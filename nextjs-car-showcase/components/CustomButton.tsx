472223dc0de454a765fb703a2fc0c042e464a57e
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2023-10-12 18:05:00 -0600

    Create the Layout
    Add the Navbar and the Footer

--- nextjs-car-showcase/components/CustomButton.tsx
+++ nextjs-car-showcase/components/CustomButton.tsx
@@ -3,11 +3,11 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = ({containerStyles, title, handleClick}: CustomButtonProps) => {
+const CustomButton = ({containerStyles, title, handleClick, btnType}: CustomButtonProps) => {
   return (
     <button 
       disabled={false}
-      type={'button'}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
