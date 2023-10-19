19714e7ebe656448839d21598ad17a2c51cf4c38
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2023-10-19 14:32:18 -0600

    Integrate API for images

--- nextjs-car-showcase/utils/index.tsx
+++ nextjs-car-showcase/utils/index.tsx
@@ -14,7 +14,7 @@ export async function fetchCars() {
 
   // Set the required headers for the API request
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`,
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera`,
     {
       headers: headers,
     }
@@ -36,3 +36,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery' || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
